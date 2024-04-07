b9e49b3c347d1231855cc2668e1e94aa03c6c1fa
marcus <mmxo5813@gmail.com>
2024-04-08 01:46:24 +1000

    logging

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -5,7 +5,8 @@ namespace IntentAPI.Models;
 public class AppDbContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseNpgsql(@"Host=host.docker.internal;Username=root;Password=root;Database=intent_db;Port=5432");
+            => options.UseNpgsql(@"Host=host.docker.internal;Username=root;Password=root;Database=intent_db;Port=5432")
+                      .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
 
     public DbSet<User> Users { get; set; }
 
@@ -15,7 +16,9 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Recurring>().Property(e => e.RecurringMode)
+        modelBuilder
+            .Entity<Recurring>()
+            .Property(e => e.RecurringMode)
             .HasConversion(
                 v => v.ToString(),
                 v => (RecurringMode)Enum.Parse(typeof(RecurringMode), v));
