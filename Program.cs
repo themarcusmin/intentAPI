529bad94e028d5972d4d7ca738ca3db603be981e
marcus <mmxo5813@gmail.com>
2024-04-08 15:36:35 +1000

    configure port, https

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+            policy.WithOrigins("http://localhost:5173", "https://intent-app.vercel.app/").AllowAnyHeader().AllowAnyMethod();
         });
 });
 
